3ea61b76040add6d9c301b6135016f3b0c2b80ab
shamouni <amin.shamounipur@gmail.com>
2023-02-07 15:13:17 +0330

    update error handle

--- src/components/ErrorHandle/ErrorHandle.tsx
+++ src/components/ErrorHandle/ErrorHandle.tsx
@@ -7,7 +7,8 @@ const ErrorHandle = (props: IProps) => {
 
   return (
     <>
-      <h1>Something went wrong</h1>
+      <h1 className="mt-10">Something went wrong!</h1>
+      <p className="mb-10">API Error: {error?.message}</p>
       <pre>{JSON.stringify(error, null, 2)}</pre>
     </>
   )
